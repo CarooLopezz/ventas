834b7af7e21f09a312af65e57863cfeaa3d25c3b
Iara <iarabecerra24@gmail.com>
2025-05-07 14:25:29 -0300

    Renombrar modelos y rutas de 'orders' a 'sales'

--- config/config.py
+++ config/config.py
@@ -7,5 +7,14 @@ user = os.getenv("MYSQL_USER")
 password = os.getenv("MYSQL_PASSWORD")
 host = os.getenv("MYSQL_HOST")
 database = os.getenv("MYSQL_DB")
+from dotenv import load_dotenv
+import os
 
-DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+load_dotenv()
+
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+database = os.getenv("MYSQL_DB")
+
+DATABASE_CONNECTION_URI = f"mysql+pymysql://{root}:{root}@{host}:3306/{database}"
