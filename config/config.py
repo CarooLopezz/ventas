f2deda95a1640832e92cd0aca799f26db2ab3726
Lara78a <magallanedlara@gmail.com>
2025-05-07 21:59:47 -0300

    proveedor

--- config/config.py
+++ config/config.py
@@ -3,9 +3,18 @@ import os
 
 load_dotenv()
 
-user = os.getenv("root")
-password = os.getenv("")
-host = os.getenv("127.0.0.1")
-database = os.getenv("ventas")
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+database = os.getenv("MYSQL_DB")
+from dotenv import load_dotenv
+import os
 
-DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+load_dotenv()
+
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+database = os.getenv("MYSQL_DB")
+
+DATABASE_CONNECTION_URI = f"mysql+pymysql://{root}:{root}@{host}:3306/{database}"
