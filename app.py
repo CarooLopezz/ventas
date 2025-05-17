d72a5d187e45ddf6ca9fb6bbe2a32f296d4e3eb7
Aeantunez <aeantunez@hotmail.com.ar>
2025-05-17 17:32:57 -0300

    .

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from config.config import DATABASE_CONNECTION_URI
 from routes.client_routes import client
+from routes.products_routes import products
 from models.db import db
 
 app = Flask(__name__)
 
 app.register_blueprint(client)
-
+app.registrer_blueprint(products)
 
 app.config["SQLALCHEMY_DATABASE_URI"]= DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -15,6 +16,7 @@ db.init_app(app)
 
 with app.app_context():
     from models.client import Client
+    from models.products import Products
     # db.drop_all()
     db.create_all()
 
